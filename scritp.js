49589e8fc5f69ac3ca3edd3148be593bbf9910bb
ArthurCosmix <158165446+ArthurCosmix@users.noreply.github.com>
2024-02-21 19:25:30 +0700

    Rock paper scissor Verson 2 add ui

--- scritp.js
+++ scritp.js
@@ -1,32 +1,55 @@
-//computer selection
+const selectionELement = document.querySelectorAll(".selection")
+const winnerLabel = document.querySelector(".winner-text")
+const playerImage = document.querySelector(".player-image")
+const computerImage = document.querySelector(".computer-image")
+const winnerBoard = document.querySelector(".winner-board")
+const playerShowScore = document.querySelector(".player-score")
+const comShowScore = document.querySelector(".computer-score")
+
+
+//store score in every round
+let playerScore = 0
+let computerScore = 0
+
+
+
+//computer selection represent from random number 0-3 and make condition to assign selection
 const computerSelection = function(){
     let comChoice = Math.floor(Math.random() * 4)
     if(comChoice == 0){
-        return "Rock"
+        return "rock"
     }else if(comChoice == 1){
-        return "Paper"
+        return "paper"
     }else{
-        return "Scissor"
+        return "scissor"
     }
 }
-//computer selection represent from random number 0-3 and make condition to assign selection
 
-//store score in every round
-const playerScore = []
-const computerScore = []
-let playerLastScore = null
-let computerLastScore = null
+
+//play function
+function play(choice){
+    const playerChoice = choice.target.id
+    const comChoice = computerSelection()
+    const winner = rockPaperScissor(playerChoice, comChoice)
+    winnerLabel.innerHTML = winner.winner
+    playerShowScore.innerHTML = `Player score : ${playerScore}`
+    comShowScore.innerHTML = `Computer score : ${computerScore}`
+    playerImage.src = `assets/${playerChoice}.png`
+    computerImage.src = `assets/${comChoice}.png`
+    winnerBoard.classList.add("active")
+
+    
+}
 
 const rockPaperScissor = function(playerSelection, computerSelection){
-    const result = {}
-    player = playerSelection.toLowerCase()
-    com = computerSelection.toLowerCase()
+    player = playerSelection
+    com = computerSelection
     if(
         player == "rock" && com == "scissor" ||
         player == "paper" && com == "rock"   ||
         player == "scissor" && com == "paper" 
     ){
-        playerScore.push(1)
+        playerScore++
        return {
         winner : "Player",
         playerSelected : player,
@@ -38,7 +61,7 @@ const rockPaperScissor = function(playerSelection, computerSelection){
         player == "scissor" && com == "rock"   ||
         player == "rock" && com == "paper" 
     ){
-        computerScore.push(1)
+        computerScore++
         return {
             winner : "Computer",
             playerSelected : player,
@@ -53,41 +76,4 @@ const rockPaperScissor = function(playerSelection, computerSelection){
     }
 }
 
-//score
-
-//playe game 5 round
-const runGame = function(){
-    for(let i = 0; i < 5; i++){
-        console.log(rockPaperScissor("rocK", computerSelection()))
-    }
-
-    playerLastScore = playerScore.reduce((total, score) => total += score , 0)
-    computerLastScore = computerScore.reduce((total, score) => total += score , 0)
-
-
-    if(playerLastScore > computerLastScore){
-        console.log({
-            theWinner : "Player",
-            comTotalScore : computerLastScore,
-            playerTotalScore : playerLastScore,
-            text : "Congratulation Player won!!"
-        })
-    }
-    else if( computerLastScore > playerLastScore){
-        console.log({
-            theWinner : "Computer",
-            comTotalScore : computerLastScore,
-            playerTotalScore : playerLastScore,
-            text : "Congratulation Computer won!!"
-        })
-    }
-    else{
-        console.log({
-            theWinner : "This competition is Draw",
-            comTotalScore : computerLastScore,
-            playerTotalScore : playerLastScore,
-            text : "Congratulation you two won!!"
-        })
-    }
-}
-runGame()
+selectionELement.forEach(choice => choice.addEventListener("click", play))
